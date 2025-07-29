a48d6b73e6e11487f232b8bcacb9cc9d41244ad4
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2025-07-29 11:28:39 +0200

    Re-deploy

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { NETWORKS } from "./lib/networks";
 
-export const LAUNCHPAD_MODULE_ADDRESS = "0x8f06ad852fa2debd2b3e8cf33b33187a162d9a88a0f253bab4d0f7f8afb215ad";
+export const LAUNCHPAD_MODULE_ADDRESS = "0x03561a6ebe3195bb5c3b3b700bdcb8d3131a388fd27f2cae39a6ac256a51fa41";
 export const COLLECTION_ID = "";
 export const SINGLE_COLLECTION_MODE = COLLECTION_ID.length > 0;
 export const MOVE_NETWORK = NETWORKS.TESTNET;
