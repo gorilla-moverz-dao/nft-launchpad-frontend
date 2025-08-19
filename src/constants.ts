b54069ee960d9e0b4536a1c07ba6c78ce9e7784e
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2025-08-19 15:22:47 +0200

    Setup ruffles layout

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { NETWORKS } from "./lib/networks";
 
 export const LAUNCHPAD_MODULE_ADDRESS = "0xb94fa3d05e668dee703ddb5bc43a92ebe1e88ba0fd9823b3cb7f679ce5f7e227";
-export const COLLECTION_ID = "";
+export const COLLECTION_ID = "0xfc7b0a61f3e6cff0bd77abe5956c4daf6c7f0b9150a4a2493d5a418e156080bf";
 export const SINGLE_COLLECTION_MODE = COLLECTION_ID.length > 0;
 export const MOVE_NETWORK = NETWORKS.TESTNET;
