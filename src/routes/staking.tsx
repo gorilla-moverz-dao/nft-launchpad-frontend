5d8c7e76f482aa7301cc0799d5c9e8bf81db7cfd
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2025-08-23 21:59:38 +0200

    Update staking

--- src/routes/staking.tsx
+++ src/routes/staking.tsx
@@ -7,6 +7,7 @@ import { STAKING_MODULE_ADDRESS } from "@/constants";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTransaction } from "@/hooks/useTransaction";
+import { useCollectionNFTs } from "@/hooks/useCollectionNFTs";
 
 export const Route = createFileRoute("/staking")({
   component: RouteComponent,
@@ -19,6 +20,8 @@ function RouteComponent() {
   const { transactionInProgress: addingCollection, executeTransaction } = useTransaction();
   const { transactionInProgress: creatingStaking, executeTransaction: executeCreateStaking } = useTransaction();
 
+  const colId = "0x83dd3df737a8e86c41a8bdd2aaf9b9bdc4ae8e8b6d08911f0259f23278ff7798";
+
   const { data: stakingInfo, refetch: refetchStakingInfo } = useQuery({
     queryKey: ["staking-info"],
     queryFn: async () => {
@@ -26,8 +29,22 @@ function RouteComponent() {
         functionArguments: [],
         typeArguments: [],
       });
+
+      const [res2] = await stakingClient.view.view_active_staking_pools({
+        functionArguments: [],
+        typeArguments: [],
+      });
+      console.log(res2);
+
       return res;
     },
+  });
+
+  // TODO: Get collection Id from the staking info? But how?
+  const { data: collectionNFTs } = useCollectionNFTs({
+    enabled: !!stakingInfo,
+    onlyOwned: true,
+    collectionIds: [colId],
   });
 
   return (
@@ -76,9 +93,8 @@ function RouteComponent() {
           }
 
           const dpr = 86400;
-          const colId = "0x4a401482b77148e0efeddc73052ac1ed7847793cdf3b2798ec461b9c0c5846ea";
-          const inititialStakingAmount = 1000000000000000000;
-          const rewardMetadataObjectAddress = "0x21d4d5406e3918c1bac02e3385e00078b8a2150e622d28bb1f66e9a38dc4750a"; // BANANA A
+          const inititialStakingAmount = BigInt(1000000000000000000);
+          const rewardMetadataObjectAddress = "0x17d965eb3affe4792b493cf5faad6c1e768bf94fe8ba846867ec3f05ac587463"; // BANANA A
 
           await executeCreateStaking(
             stakingWalletClient.create_staking({
@@ -90,6 +106,33 @@ function RouteComponent() {
       >
         {creatingStaking ? "Creating..." : "Create staking"}
       </Button>
+
+      <div>
+        <h2 className="text-2xl font-bold">Stake NFTs</h2>
+        <div className="flex flex-col gap-2">
+          {collectionNFTs?.current_token_ownerships_v2.map((item) => (
+            <div key={item.token_data_id} className="flex flex-row gap-2">
+              <div>{item.token_data_id}</div>
+              <Button
+                key={item.token_data_id}
+                onClick={() => {
+                  if (!stakingWalletClient) {
+                    return;
+                  }
+                  executeTransaction(
+                    stakingWalletClient.stake_token({
+                      arguments: [item.token_data_id as `0x${string}`],
+                      type_arguments: [],
+                    }),
+                  );
+                }}
+              >
+                Stake
+              </Button>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
