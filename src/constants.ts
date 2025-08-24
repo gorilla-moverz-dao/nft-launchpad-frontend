929fce6713f14ddf63e08e5e1dbf9a9a7c67593a
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2025-08-24 09:54:05 +0200

    Update staking contract

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { NETWORKS } from "./lib/networks";
 
 export const LAUNCHPAD_MODULE_ADDRESS = "0xb94fa3d05e668dee703ddb5bc43a92ebe1e88ba0fd9823b3cb7f679ce5f7e227";
-export const STAKING_MODULE_ADDRESS = "0x21ca7ca53557bd1587ff7fe518d64cdba84adcfb0d9c083196a407180652eeac";
+export const STAKING_MODULE_ADDRESS = "0x13c4efe58d6782fb9116f103b6123a4a09ce6ecb66ec1363a65b3bd3bbd8a0f7";
 export const COLLECTION_ID = "";
 export const SINGLE_COLLECTION_MODE = COLLECTION_ID.length > 0;
 export const MOVE_NETWORK = NETWORKS.TESTNET;
