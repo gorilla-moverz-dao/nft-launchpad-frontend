2975b27f6a40b84146e896b3d821f5c2606c50f9
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2025-09-18 10:32:38 +0200

    Update config

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,9 @@
 import { NETWORKS } from "./lib/networks";
 
-export const LAUNCHPAD_MODULE_ADDRESS = "0xb94fa3d05e668dee703ddb5bc43a92ebe1e88ba0fd9823b3cb7f679ce5f7e227";
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+const NETWORK = import.meta.env?.VITE_NETWORK || "TESTNET";
+
+export const LAUNCHPAD_MODULE_ADDRESS = "0xe4fa259d7f337bf12723b7fc3606399955758cdef49a0303ce8d265f6dbe7e71";
 export const COLLECTION_ID = "";
 export const SINGLE_COLLECTION_MODE = COLLECTION_ID.length > 0;
-export const MOVE_NETWORK = NETWORKS.TESTNET;
+export const MOVE_NETWORK = NETWORKS[NETWORK as keyof typeof NETWORKS];
--- src/lib/aptos.ts
+++ src/lib/aptos.ts
@@ -1,6 +1,6 @@
 import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 import { createSurfClient } from "@thalalabs/surf";
-import { MOVE_NETWORK } from "@/constants";
+import { LAUNCHPAD_MODULE_ADDRESS, MOVE_NETWORK } from "@/constants";
 import { ABI as launchpadABI } from "@/abi/nft_launchpad";
 import { ABI as nftReductionManagerABI } from "@/abi/nft_reduction_manager";
 
@@ -14,8 +14,8 @@ const config = new AptosConfig({
 
 // Initialize client
 export const aptos = new Aptos(config);
-export const launchpadClient = createSurfClient(aptos).useABI(launchpadABI);
-export const nftReductionManagerClient = createSurfClient(aptos).useABI(nftReductionManagerABI);
+export const launchpadClient = createSurfClient(aptos).useABI(launchpadABI, LAUNCHPAD_MODULE_ADDRESS);
+export const nftReductionManagerClient = createSurfClient(aptos).useABI(nftReductionManagerABI, LAUNCHPAD_MODULE_ADDRESS);
 
 // Helper function to get account balance
 export const getAccountBalance = async (address: string) => {
