bff7201dce44740415d303e88daaa1ca6cdead4b
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2025-09-18 10:43:51 +0200

    Fix address

--- src/hooks/useClients.ts
+++ src/hooks/useClients.ts
@@ -2,14 +2,14 @@ import { useWalletClient } from "@thalalabs/surf/hooks";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { ABI as coinABI } from "@/abi/coin";
 import { ABI as launchpadABI } from "@/abi/nft_launchpad";
-import { MOVE_NETWORK } from "@/constants";
+import { LAUNCHPAD_MODULE_ADDRESS, MOVE_NETWORK } from "@/constants";
 
 export function useClients() {
   const { client } = useWalletClient();
   const { account, connected, network } = useWallet();
 
   const coinClient = client?.useABI(coinABI);
-  const launchpadClient = client?.useABI(launchpadABI);
+  const launchpadClient = client?.useABI({ ...launchpadABI, address: LAUNCHPAD_MODULE_ADDRESS });
 
   const correctNetwork = network?.chainId === MOVE_NETWORK.chainId;
 
